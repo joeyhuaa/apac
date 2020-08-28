0e995940477222efa62c130b6a939caad097bf1f
joeyhuaa <joeyhua17@gmail.com>
2020-08-27 17:13:07 -0700

    Updates

---
+++ precache-manifest.00eb1a2469476dc832dfd70d0efed3d6.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0b3d7d6b16f4d85c24dc3fb759c9ffb",
+    "url": "/apac/index.html"
+  },
+  {
+    "revision": "370967e83b286902ddbb",
+    "url": "/apac/static/css/main.32227a23.chunk.css"
+  },
+  {
+    "revision": "7cbc82fdf87d0f2cc3b9",
+    "url": "/apac/static/js/2.b72c003b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/apac/static/js/2.b72c003b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "370967e83b286902ddbb",
+    "url": "/apac/static/js/main.049021df.chunk.js"
+  },
+  {
+    "revision": "fefb3b02317b5877957f",
+    "url": "/apac/static/js/runtime-main.adfb2399.js"
+  },
+  {
+    "revision": "ffdb8863c0c3a1194dbc06746ae6350e",
+    "url": "/apac/static/media/DEPTS BG.ffdb8863.jpg"
+  },
+  {
+    "revision": "805180e5fc4c740f3dc13c2e2d63525f",
+    "url": "/apac/static/media/art.805180e5.jpg"
+  },
+  {
+    "revision": "e412b10f1aa311188f775b1001178ba8",
+    "url": "/apac/static/media/logo.e412b10f.png"
+  },
+  {
+    "revision": "7bb750a6cbe371241574568b4cc6accf",
+    "url": "/apac/static/media/menu.7bb750a6.png"
+  },
+  {
+    "revision": "00cd2609c442dcd327f23437ebc9f95e",
+    "url": "/apac/static/media/mural.00cd2609.jpg"
+  },
+  {
+    "revision": "76de6cd79b0fa503302144b8be20a6ec",
+    "url": "/apac/static/media/paint splotch.76de6cd7.jpg"
+  },
+  {
+    "revision": "9ece69f8a3f00d98995876b76511fad7",
+    "url": "/apac/static/media/pic1.9ece69f8.jpg"
+  },
+  {
+    "revision": "8034f27cb497114dc3b01d8d4a716dca",
+    "url": "/apac/static/media/pic2.8034f27c.jpg"
+  },
+  {
+    "revision": "48785c255fdcf1c837841eea24e667b7",
+    "url": "/apac/static/media/pic3.48785c25.jpg"
+  },
+  {
+    "revision": "11a7f9f955c74af5037d647444cac1af",
+    "url": "/apac/static/media/pic4.11a7f9f9.jpg"
+  },
+  {
+    "revision": "040cc7e468465ba6b378bcc0d419f83e",
+    "url": "/apac/static/media/pic5.040cc7e4.jpg"
+  },
+  {
+    "revision": "0eeebab2e08a7eb6db82144db53ec5ea",
+    "url": "/apac/static/media/pic6.0eeebab2.jpg"
+  }
+]);
