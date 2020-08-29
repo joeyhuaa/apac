3ca8bfc798218b8cf080c56b189b42d346face1d
joeyhuaa <joeyhua17@gmail.com>
2020-08-29 16:13:24 -0700

    event blur mobile

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -8,7 +8,16 @@ export default function DeptCard({
   return (
     <div className='event-card' style={{backgroundImage: `url(${background})`}}>
       <div className='blur' style={{backgroundImage: `url(${background})`}} />
-      <div className='inner-white translucent'>
+      <div className='inner-white'>
+        <h4 style={{
+          fontFamily:'Poppins', 
+          textAlign:'center'
+        }}>{title}</h4>
+        <p style={{
+          marginTop:'2em'
+        }}>{text}</p>
+      </div>
+      <div className='inner-white-mobile'>
         <h4 style={{
           fontFamily:'Poppins', 
           textAlign:'center'
